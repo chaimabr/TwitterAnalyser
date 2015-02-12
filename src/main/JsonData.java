e045a176b78630265b45e1a026a65e1c0b95847a
chaimabr <chaima.benrabah@gmail.com>
2015-02-12 10:35:49 +0100

    update the code to generate text files containing keywords

--- src/main/JsonData.java
+++ src/main/JsonData.java
@@ -13,14 +13,18 @@ public class JsonData {
 	
 	//list count and text
 	static List<String> listText = new ArrayList<String>();
-	static List<Long> listCount = new LinkedList<Long>();
-	static List<Long> listCountTrier = new LinkedList<Long>();
+	static List<Long> listRtCount = new LinkedList<Long>();
+	static List<Long> listRtCountTrier = new LinkedList<Long>();
+	static List<String> stopWords = Arrays.asList("i","a","its","so","about","than","other","more","an","and","are","as","at","be","by","com","de","en","for","from","how","in","is","it","la","of","on","or","that","the","this","to","was","what","when","where","who","will","with","und","the","www");  
 	
 	public static void main(String[] args) {
 		
 		try {
 			// read the json file
-			BufferedReader buffer = new BufferedReader(new FileReader("D:\\projeteurecom\\workspace\\TwitterAnalyser\\test1.json"));
+			BufferedReader buffer = new BufferedReader(new FileReader("D:\\dataset\\Tweet_files\\5_5_2012_18_9.json"));
+			
+			FileWriter fileWrite = new FileWriter(new File("5_5_2012_18_9.txt"));
+			BufferedWriter bufferWr = new BufferedWriter(fileWrite);
 			
 			while((str = buffer.readLine()) != null) 
 			{				
@@ -32,31 +36,68 @@ public class JsonData {
 				//System.out.println("The text is: " + (String)jsonObject.get("text"));
 				
 				// get favorites count and store value
-				JSONObject user = (JSONObject)jsonObject.get("user");
-				listCount.add((Long) user.get("favourites_count"));	
-				listCountTrier.add((Long) user.get("favourites_count"));	
+				
+				listRtCount.add((Long)jsonObject.get("retweet_count"));
+				listRtCountTrier.add((Long)jsonObject.get("retweet_count"));
+				
 			} 
 			buffer.close(); 
 			
-			//trier top 5
-			Collections.sort(listCountTrier);
+			//sort the list
+			Collections.sort(listRtCountTrier);
 						
-			//afficher les top 5		
+			//display top 2		
 			
-			System.out.println("Top Five tweets according how much they were favorited are :");
-					
-			for (int i=0;i<listCount.size();i++)
+			for (int i=0;i<listRtCount.size();i++)
 			{
-				for (int j=listCountTrier.size()-1;j>listCountTrier.size()-6;j--)
+				for (int j=listRtCountTrier.size()-1;j>listRtCountTrier.size()-20;j--)
 				{
-					if (listCount.get(i) == listCountTrier.get(j))
+					if (listRtCount.get(i) == listRtCountTrier.get(j))
 					{
-						System.out.println("Text du tweet " + (listCountTrier.size()-1-j) + " is : " + listText.get(i));
-						System.out.println("number of favourites " + (listCountTrier.size()-1-j) + " is : " + listCount.get(i));
+						 String s= listText.get(i);
+					     String[] words = s.split(" ");
+					     words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+					 	 ArrayList<String> keywords = new ArrayList<String>();
+
+					     for(String word : words)
+					     {
+					         String wordCompare = word.toLowerCase();
+					         if(!stopWords.contains(wordCompare) && !keywords.contains(wordCompare))
+					         {
+					             keywords.add(word);
+					         }
+					     }
+
+					        for (String str : keywords){
+					        	if (str.length()>3){
+					        	bufferWr.write(str+" "); }
+					        }
+					        bufferWr.newLine();
 					}
 				}
 			}
-			
+			bufferWr.close();
+			fileWrite.close();
+			File inputFile = new File("5_5_2012_18_9.txt");
+			File tempFile = new File("myTempFile.txt");
+
+			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+			int k=0;
+			String previousline = reader.readLine();
+			writer.write(previousline + System.getProperty("line.separator"));
+			String currentLine;
+
+			while(k==0 && (currentLine = reader.readLine()) != null) {
+			    String trimmedLine = currentLine.trim();
+			    if(trimmedLine.equals(previousline)) continue;
+			    writer.write(currentLine + System.getProperty("line.separator"));
+			    k++;
+			}
+			writer.close(); 
+			reader.close(); 
+			boolean success = tempFile.renameTo(inputFile);
 		} catch (Exception ex)
 		{
 			ex.printStackTrace();
