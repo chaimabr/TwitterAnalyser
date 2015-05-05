858a7676d638656f7e7910f9c8b479e2f468473e
chaimabr <chaima.benrabah@gmail.com>
2015-05-05 15:19:36 +0200

    remove stop words

--- src/main/JsonData.java
+++ src/main/JsonData.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
@@ -15,15 +17,21 @@ public class JsonData {
 	static List<String> listText = new ArrayList<String>();
 	static List<Long> listRtCount = new LinkedList<Long>();
 	static List<Long> listRtCountTrier = new LinkedList<Long>();
-	static List<String> stopWords = Arrays.asList("i","a","its","so","about","than","other","more","an","and","are","as","at","be","by","com","de","en","for","from","how","in","is","it","la","of","on","or","that","the","this","to","was","what","when","where","who","will","with","und","the","www");  
+	static List<String> stopWords ;
+	
 	
 	public static void main(String[] args) {
 		
 		try {
+			 BufferedReader sw1 = new BufferedReader(new FileReader(""+"stopwords.txt"));
+		        while((str = sw1.readLine()) != null) 
+				{	
+					stopWords.add(str);
+				}
 			// read the json file
-			BufferedReader buffer = new BufferedReader(new FileReader("D:\\dataset\\Tweet_files\\5_5_2012_18_9.json"));
+			BufferedReader buffer = new BufferedReader(new FileReader("D:\\dataset\\Tweet_files\\5_5_2012_16_16.json"));
 			
-			FileWriter fileWrite = new FileWriter(new File("5_5_2012_18_9.txt"));
+			FileWriter fileWrite = new FileWriter(new File("5_5_2012_16_16.txt"));
 			BufferedWriter bufferWr = new BufferedWriter(fileWrite);
 			
 			while((str = buffer.readLine()) != null) 
@@ -47,26 +55,35 @@ public class JsonData {
 			Collections.sort(listRtCountTrier);
 						
 			//display top 2		
-			
+			int l=0;
 			for (int i=0;i<listRtCount.size();i++)
 			{
 				for (int j=listRtCountTrier.size()-1;j>listRtCountTrier.size()-20;j--)
 				{
 					if (listRtCount.get(i) == listRtCountTrier.get(j))
 					{
+						l++;
 						 String s= listText.get(i);
+						 String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+	                     Pattern pa = Pattern.compile(urlPattern,Pattern.CASE_INSENSITIVE);
+	                     Matcher m = pa.matcher(s);
+	                     int f = 0;
+	                     while (m.find()) {
+	                         s = s.replaceAll(m.group(f),"").trim();
+	                         f++;
+	                     }
 					     String[] words = s.split(" ");
 					     words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
 					 	 ArrayList<String> keywords = new ArrayList<String>();
 
 					     for(String word : words)
 					     {
-					         String wordCompare = word.toLowerCase();
-					         if(!stopWords.contains(wordCompare) && !keywords.contains(wordCompare))
+					         if((!(word.charAt(0)=='@')) && ((word.charAt(0)>='A' && word.charAt(0)<='Z') || (!stopWords.contains(word.toLowerCase())) && !keywords.contains(word.toLowerCase())))
 					         {
 					             keywords.add(word);
 					         }
 					     }
+					    
 
 					        for (String str : keywords){
 					        	if (str.length()>3){
@@ -78,7 +95,7 @@ public class JsonData {
 			}
 			bufferWr.close();
 			fileWrite.close();
-			File inputFile = new File("5_5_2012_18_9.txt");
+			File inputFile = new File("5_5_2012_16_16.txt");
 			File tempFile = new File("myTempFile.txt");
 
 			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
